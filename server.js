8a2603f2552683850203376a9d954172bb326c90
Benjamin White <benjamin.e.white1@gmail.com>
2016-12-07 16:39:34 -0500

    use global promises

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Mongoose internally uses a promise-like object,
+// but its better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
 const {PORT, DATABASE_URL} = require('./config');
@@ -19,50 +23,72 @@ app.get('/restaurants', (req, res) => {
     // documents, and that's too much to process/return
     .limit(10)
     // `exec` returns a promise
-    .exec()
-    .then(
-      // success callback
-      restaurants => res.json({restaurants}),
-      // failure callback
+    // .exec()
+    // success callback: for each restaurant we got back, we'll
+    // call the `.apiRepr` instance method we've created in
+    // models.js in order to only expose the data we want the API return.
+    .then(restaurants => {
+      res.json({
+        restaurants: restaurants.map(
+          (restaurant) => restaurant.apiRepr())
+      });
+    })
+    .catch(
       err => {
         console.error(err);
         res.status(500).json({message: 'Internal server error'});
-      }
-    )}
-);
+    });
+});
 
 // can also request by ID
 app.get('/restaurants/:id', (req, res) => {
   Restaurant
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
     .findById(req.params.id)
     .exec()
-    .then(
-      restaurant => res.json(restaurant),
-      err => {
-        console.error(err);
-        res.status(500).json({message: 'Internal server error'})}
-    );
+    .then(restaurant =>res.json(restaurant.apiRepr()))
+    .catch(err => {
+      console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    });
 });
 
 
 app.post('/restaurants', (req, res) => {
-  const {name, borough, restaurant_id, cuisine, grades, address} = req.body;
+
+  const requiredFields = ['name', 'borough', 'cuisine'];
+  requiredFields.forEach(field => {
+    // ensure that required fields have been sent over
+    if (! (field in req.body && req.body[field])) {
+      res.status(400).json({message: `Must specify value for ${field}`});
+    }
+  });
 
   Restaurant
-    .create({name, borough, restaurant_id, cuisine, grades, address})
+    .create({
+      name: req.body.name,
+      borough: req.body.borough,
+      cuisine: req.body.cuisine,
+      grades: req.body.grades,
+      address: req.body.address})
     .then(
-      restaurant => res.status(201).json(restaurant),
-      err => res.status(500).json({message: 'Internal server error'}));
+      restaurant => res.status(201).json(restaurant.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
 });
 
 
 app.put('/restaurants/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
       `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
-    return res.status(400).json({message: message});
+    res.status(400).json({message: message});
   }
   // N.B. -- this assumes that clients are always sending over existing or
   // updated data for these fields.
@@ -73,21 +99,17 @@ app.put('/restaurants/:id', (req, res) => {
         borough: req.body.borough,
         cuisine: req.body.cuisine,
         address: req.body.address,
-        grades: req.body.ratings,
-        restaurant_id: req.body.restaurant_id
       }
     })
-    .then(
-      restaurant => res.status(204).end(),
-      err => res.status(500).json({message: 'Internal server error'}));
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 app.delete('/restaurants/:id', (req, res) => {
   Restaurant
     .findByIdAndRemove(req.params.id)
-    .then(
-      restaurant => res.status(204).end(),
-      err => res.status(500).json({message: 'Internal server error'}));
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 // catch-all endpoint if client makes request to non-existent endpoint

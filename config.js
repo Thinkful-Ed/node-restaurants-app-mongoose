7663340869ccae008e00278045c6c077232d7e96
Benjamin White <benjamin.e.white1@gmail.com>
2016-12-07 12:12:10 -0500

    update config and add required fields

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/shopping-list';
+                      'mongodb://localhost/restaurants-app';
 exports.PORT = process.env.PORT || 8080;
--- models.js
+++ models.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 // this is our schema to represent a restaurant
 const restaurantSchema = mongoose.Schema({
-  name: String,
+  name: {type: String, required: true},
   restaurant_id: String,
   borough: {type: String, required: true},
-  cuisine: String,
+  cuisine: {type: String, required: true},
   address: {
     building: String,
     coord: [String],

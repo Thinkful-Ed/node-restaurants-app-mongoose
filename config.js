0c4de3ea7d301e92b1f1be275f03cb407e54c430
Chris Tsongas <chris.tsongas@gmail.com>
2018-01-13 21:12:21 +0000

    do not default to production database

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/restaurants-test';
 exports.PORT = process.env.PORT || 8080;
